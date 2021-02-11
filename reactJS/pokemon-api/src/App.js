1064387f67c2b7691b367f2a601ebfc70bd770e1
fersayago <sayago.fernando@gmail.com>
2021-02-11 02:44:09 -0300

    Comenzado proyecto PokeAPI en reactJS

---
+++ reactJS/pokemon-api/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import "./components/Navbar";
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <div className="App">
+        <h1>Hello Fer</h1>
+        <h2>Start editing to see some magic happen</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
